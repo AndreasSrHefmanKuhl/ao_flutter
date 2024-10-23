2faf2c60d6375626a040117da612f1ab254c92c7
AndreasKuhl <hefbolzen@gmail.com>
2024-10-23 10:16:44 +0200

    made ultimate Button

--- lib/widgets/ult_button.dart
+++ lib/widgets/ult_button.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
 class UltButton extends StatelessWidget {
+  final Color bgColor;
+  final double borderWidth;
+  final double borderRadius;
+  final String text;
+  final Color textColor;
+  final double btnHeight;
+  final double btnWidth;
+
   const UltButton({
     super.key,
+    required this.bgColor,
+    required this.borderWidth,
+    required this.borderRadius,
+    required this.text,
+    required this.textColor,
+    required this.btnHeight,
+    required this.btnWidth,
   });
 
   @override
@@ -10,14 +25,14 @@ class UltButton extends StatelessWidget {
     return Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(
-            color: Colors.transparent,
-            border: Border.all(width: 1),
-            borderRadius: const BorderRadius.all(Radius.circular(10))),
-        height: 35,
-        width: 250,
-        child: const Text(
-          ' register urself for membership',
-          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+            color: bgColor,
+            border: Border.all(width: borderWidth),
+            borderRadius: BorderRadius.all(Radius.circular(borderRadius))),
+        height: btnHeight,
+        width: btnWidth,
+        child: Text(
+          text,
+          style: TextStyle(color: textColor),
           textAlign: TextAlign.center,
         ));
   }
