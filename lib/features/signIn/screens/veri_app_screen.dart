847fc0e110a407ff6863fb288d47753f1171a9b4
AndreasKuhl <hefbolzen@gmail.com>
2024-10-25 12:20:20 +0200

    fin layout sign in flow

--- lib/features/signIn/screens/veri_app_screen.dart
+++ lib/features/signIn/screens/veri_app_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ao/features/signIn/widgets/ult_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,13 +18,33 @@ class VeriScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           Positioned(
+              top: 150,
+              left: 22,
               child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                Text('Welcome!',
-                    style:
-                        TextStyle(fontSize: 65, fontWeight: FontWeight.bold)),
-              ])),
+                    Text('Welcome!',
+                        style: TextStyle(
+                            fontSize: 65, fontWeight: FontWeight.bold)),
+                    Text('U have been successfully registered!',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ])),
+          Positioned(
+              bottom: 60,
+              left: 120,
+              child: UltButton(
+                  bgColor: Colors.white54,
+                  borderWidth: 1,
+                  borderRadius: 12,
+                  text: 'Main Menu',
+                  textColor: Colors.black54,
+                  btnHeight: 50,
+                  btnWidth: 150,
+                  borderColor: Colors.black54)),
         ]),
       ),
     );
