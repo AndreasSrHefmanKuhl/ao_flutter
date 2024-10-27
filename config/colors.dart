84b11b52e8d9452c479ebaf981e7827aad8004f6
AndreasKuhl <hefbolzen@gmail.com>
2024-10-27 17:42:40 +0100

    finb siztes and colors data for theme

---
+++ config/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color borderBlack = Color.fromARGB(255, 0, 0, 0);
+const Color borderWhite = Color.fromARGB(255, 250, 248, 248);
+const Color bgColor = Color.fromARGB(0, 0, 0, 0);
---
+++ config/sizes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const double signInWidth = 1;
+const double signInRadius = 12;
+
+const double headlineTextSize = 38;
+const double signInContainerHeight = 60;
+const double signInContainerWidth = 60;
+
+const double minDistance = 10;
+const double smallDistance = 30;
+const double medDistance = 60;
+
+const minVerticalSpace = SizedBox(height: minDistance);
+const smallVerticalSpace = SizedBox(height: smallDistance);
+const mediumVerticalSpace = SizedBox(height: medDistance);
+
+const double btnHeight = 35;
+const double btnWidth = 200;
+const double btnBorderRadius = 10;
+const double btnBorderWidth = 1;
