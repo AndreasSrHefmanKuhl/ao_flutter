6b339cac18c13ab13e67a3ef3d14ca1e7b4db4a1
AndreasKuhl <hefbolzen@gmail.com>
2024-11-01 13:17:23 +0100

    .

--- lib/features/main_menu/widgets/menu_icon.dart
+++ lib/features/main_menu/widgets/menu_icon.dart
@@ -9,15 +9,16 @@ class MenuIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
           border: Border.all(width: 2),
           borderRadius: const BorderRadius.all(Radius.circular(14))),
-      height: 50,
-      width: 50,
+      height: 200,
+      width: 180,
       child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text(text), icon]),
+          children: [Expanded(flex: 1, child: icon)]),
     );
   }
 }
