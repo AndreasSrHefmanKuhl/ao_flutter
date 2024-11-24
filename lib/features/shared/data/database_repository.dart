08b8290bdff963b266dfb6a70ccbb7d912be74de
AndreasKuhl <hefbolzen@gmail.com>
2024-11-24 18:28:49 +0100

    finished repo #

---
+++ lib/features/shared/data/database_repository.dart
@@ -0,0 +1,26 @@
+import 'package:ao/features/shared/models/messages.dart';
+import 'package:ao/features/shared/models/user.dart';
+
+abstract class DatabaseRepository {
+  // Benutzer
+
+  Future<List<User>> getAllUsers();
+
+  Future registerUser(User user);
+
+  Future<bool> deleteUser(String userId);
+
+  Future<bool> isLoggedIn(String username, String password);
+
+  // Nachrichten
+
+  Future<String> sendMessage(Message message);
+
+  Future<void> deleteMessage(String messageId);
+
+  Future<List<Message>> getAllMessages();
+
+  Future<void> botFace();
+
+  Future<void> botChat(String userInput);
+}
---
+++ lib/features/shared/data/mockdatabase_repository.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'package:ao/features/shared/data/database_repository.dart';
+import 'package:ao/features/shared/models/messages.dart';
+import 'package:ao/features/shared/models/user.dart';
+
+class MockDatabaseRepository implements DatabaseRepository {
+  late final User currentUser;
+  late final Message currentMessage;
+  final List<User> _users = [];
+  final List<Message> _messages = [];
+
+  @override
+  Future<List<User>> getAllUsers() async {
+    await Future.delayed(Duration(seconds: 1));
+    return _users;
+  }
+
+  @override
+  Future<void> registerUser(currentUser) async {
+    await Future.delayed(Duration(seconds: 1));
+    _users.add(currentUser);
+  }
+
+  @override
+  Future<bool> deleteUser(String currentUser) async {
+    await Future.delayed(Duration(seconds: 1));
+    _users.removeWhere((user) => user.userId == currentUser);
+    return true;
+  }
+
+  @override
+  Future<bool> isLoggedIn(String username, String password) async {
+    await Future.delayed(Duration(seconds: 1));
+    return _users
+        .any((user) => user.name == username && user.password == password);
+  }
+
+  @override
+  Future<String> sendMessage(Message currentMessage) async {
+    await Future.delayed(Duration(seconds: 1));
+    _messages.add(currentMessage);
+  }
+
+  @override
+  Future<void> deleteMessage(String messageId) async {
+    await Future.delayed(Duration(seconds: 1));
+    _messages.removeWhere((message) => message.id == messageId);
+  }
+
+  @override
+  Future<List<Message>> getAllMessages() async {
+    await Future.delayed(Duration(seconds: 1));
+    return _messages;
+  }
+
+  @override
+  Future<void> botFace() async {
+    await Future.delayed(Duration(seconds: 1));
+    print('Bot-Gesicht wird angezeigt');
+  }
+
+  @override
+  Future<void> botChat(String userInput) async {
+    await Future.delayed(Duration(seconds: 1));
+    print('Bot: Hallo! Du hast geschrieben: $userInput');
+
+    _messages.add(Message(
+      sender: 'Bot',
+      content: 'Deine Nachricht wird verarbeitet...',
+      id: '',
+    ));
+  }
+}
