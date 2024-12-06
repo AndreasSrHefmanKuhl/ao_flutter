b5123841aff4338645d07851ef2edb29cd58880d
AndreasKuhl <hefbolzen@gmail.com>
2024-12-06 10:03:26 +0100

    .

--- lib/features/signIn/screens/sign_in_screen.dart
+++ lib/features/signIn/screens/sign_in_screen.dart
@@ -1,6 +1,6 @@
 import 'package:ao/config/colors.dart';
 import 'package:ao/config/sizes.dart';
-import 'package:ao/config/themes.dart';
+
 import 'package:ao/features/signIn/screens/veri_app_screen.dart';
 import 'package:ao/features/signIn/widgets/input_field.dart';
 import 'package:ao/features/signIn/widgets/sign_in_row.dart';
