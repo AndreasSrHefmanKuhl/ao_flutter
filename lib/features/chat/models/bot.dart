6b46ee1fe1a2c3cf2aec1116c2d2d05441292cc4
AndreasKuhl <hefbolzen@gmail.com>
2024-12-09 07:55:10 +0100

    bot can talk

--- lib/features/chat/models/bot.dart
+++ lib/features/chat/models/bot.dart
@@ -1,7 +1,4 @@
 class Bot {
- 
-  
-
   List<String> health = [
     "wie geht es dir?",
     "was geht?",
@@ -41,33 +38,29 @@ class Bot {
     "Kennst du die Antwort auf alles Fragen?",
   ];
 
- Future<String> botAnswer(String userInput)async {
-   userInput = userInput.toString();
-
+  Future<String> botAnswer(String userInput) async {
+    userInput = userInput.toString();
 
     if (userInput.contains("beende") && userInput.contains("Beende")) {
-       return  ("Bis zum nächsten Mal!");
-        
-      }
+      return ("Bis zum nächsten Mal!");
+    }
 
-      // weitere themen können sein... wer hat dich gebaut? ,was machst du?, Hobby?,
-      //  erzähl mir einen witz. Was ist der Sinn des Lebens?(42)
-      // implementierung von verschiedenen launen durch if else funktionsblock in klasse auslagern und durch zusätzliche if reaktion simulieren .
+    // weitere themen können sein... wer hat dich gebaut? ,was machst du?, Hobby?,
+    //  erzähl mir einen witz. Was ist der Sinn des Lebens?(42)
+    // implementierung von verschiedenen launen durch if else funktionsblock in klasse auslagern und durch zusätzliche if reaktion simulieren .
 
-      else if (health.any((element) => userInput.contains(element))) {
-        return ("Mir geht es gut, danke! Und dir?");
-      } else if (weather.any((element) => userInput.contains(element))) {
-        return("Das Wetter ist heute schön, oder?");
-      } else if (dumb.any((element) => userInput.contains(element))) {
-        return("Dumm ist der, der dummes tut ; )... mein lieber Freund! ");
-      } else if (actives.any((element) => userInput.contains(element))) {
-        return(
-            "Ich stricke am liebsten in meiner Freizeit! Die Ruhe und die Ausgeglichenheit dabei");
-      } else if (sense.any((element) => userInput.contains(element))) {
-       return("42!");
-      } else {
-        return ("Das ist interessant. Kannst du mir mehr darüber erzählen?");
-      }
+    else if (health.any((element) => userInput.contains(element))) {
+      return "Mir geht es gut, danke! Und dir?";
+    } else if (weather.any((element) => userInput.contains(element))) {
+      return "Das Wetter ist heute schön, oder?";
+    } else if (dumb.any((element) => userInput.contains(element))) {
+      return "Dumm ist der, der dummes tut ; )... mein lieber Freund! ";
+    } else if (actives.any((element) => userInput.contains(element))) {
+      return "Ich stricke am liebsten in meiner Freizeit! Die Ruhe und die Ausgeglichenheit dabei";
+    } else if (sense.any((element) => userInput.contains(element))) {
+      return "42!";
+    } else {
+      return "Das ist interessant. Kannst du mir mehr darüber erzählen?";
     }
   }
 }
