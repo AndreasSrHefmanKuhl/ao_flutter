fab76bc906fe2398ce23f3bcdf24de101c669a76
AndreasKuhl <hefbolzen@gmail.com>
2025-02-23 19:46:03 +0100

    finish agical button

--- lib/features/main_menu/widgets/radial_animation.dart
+++ lib/features/main_menu/widgets/radial_animation.dart
@@ -1,24 +1,97 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:vector_math/vector_math_64.dart' show radians;
+import 'package:vector_math/vector_math_64.dart' as vector_math;
 
 class RadialAnimation extends StatelessWidget {
+  RadialAnimation({super.key, required this.controller})
+      : scale = Tween<double>(
+          begin: 1.5,
+          end: 0.0,
+        ).animate(
+          CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn),
+        ),
+        translation = Tween<double>(
+          begin: 0.0,
+          end: 100.0,
+        ).animate(
+          CurvedAnimation(parent: controller, curve: Curves.linear),
+        ),
+        rotation = Tween<double>(
+          begin: 0.0,
+          end: 360.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.3,
+              0.9,
+              curve: Curves.decelerate,
+            ),
+          ),
+        );
+
   final AnimationController controller;
-  const RadialAnimation({required Key key, required this.controller})
-      : super(key: key);
+  final Animation<double> scale;
+  final Animation<double> translation;
+  final Animation<double> rotation;
 
   @override
-  build(BuildContext context) {
+  build(context) {
     return AnimatedBuilder(
-        animation: controller, builder: (context, builder) {
-          return Stack(alignment:Alignment.center,
-          children : [ FloatingActionButton(child:Icon(FontAwesomeIcons.walkieTalkie) )] ))});
+        animation: controller,
+        builder: (context, builder) {
+          return Stack(alignment: Alignment.center, children: [
+            _buildButton(0,
+                color: Colors.red, icon: FontAwesomeIcons.thumbtack),
+            _buildButton(45,
+                color: Colors.green, icon: FontAwesomeIcons.sprayCan),
+            _buildButton(90, color: Colors.orange, icon: FontAwesomeIcons.fire),
+            _buildButton(135,
+                color: Colors.blue, icon: FontAwesomeIcons.kiwiBird),
+            _buildButton(180, color: Colors.black, icon: FontAwesomeIcons.cat),
+            _buildButton(225, color: Colors.indigo, icon: FontAwesomeIcons.paw),
+            _buildButton(270, color: Colors.pink, icon: FontAwesomeIcons.bong),
+            _buildButton(315,
+                color: Colors.yellow, icon: FontAwesomeIcons.bolt),
+            Transform.scale(
+              scale: scale.value -
+                  1.5, // subtract the beginning value to run the opposite animation
+              child: FloatingActionButton(
+                  onPressed: _close,
+                  backgroundColor: Colors.red,
+                  child: const Icon(FontAwesomeIcons.circleXmark)),
+            ),
+            Transform.scale(
+              scale: scale.value,
+              child: FloatingActionButton(
+                  onPressed: _open,
+                  child: const Icon(FontAwesomeIcons.solidCircleDot)),
+            )
+          ]);
+        });
+  }
 
-    _open() {
-      controller.forward();
-    }
+  _buildButton(double angle, {Color? color, IconData? icon}) {
+    final double rad = radians(angle);
+    return Transform(
+        transform: vector_math.Matrix4.identity()
+          ..translate(
+              (translation.value) * cos(rad), (translation.value) * sin(rad)),
+        child: FloatingActionButton(
+            backgroundColor: color,
+            onPressed: _close,
+            elevation: 0,
+            child: Icon(icon)));
+  }
 
-    _close() {
-      controller.reverse();
-    }
+  _open() {
+    controller.forward();
+  }
+
+  _close() {
+    controller.reverse();
   }
 }
--- lib/features/main_menu/widgets/radial_menu.dart
+++ lib/features/main_menu/widgets/radial_menu.dart
@@ -1,8 +1,23 @@
+import 'package:ao/features/main_menu/widgets/radial_animation.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+void main() => runApp(const MyApp());
+
+// The parent Material App
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Scaffold(body: SizedBox.expand(child: RadialMenu())));
+  }
+}
+
+// The stateful widget + animation controller
 class RadialMenu extends StatefulWidget {
   const RadialMenu({super.key});
+
   @override
   createState() => _RadialMenuState();
 }
@@ -10,40 +25,12 @@ class RadialMenu extends StatefulWidget {
 class _RadialMenuState extends State<RadialMenu>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-      duration: const Duration(milliseconds: 900),
-      vsync: this,
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
-    return RadialAnimation(
-        controller: controller); // Replace with your widget tree
+    return RadialAnimation(controller: controller);
   }
 }
 
-class RadialAnimation extends StatelessWidget {
-  final AnimationController controller;
-  const RadialAnimation({required Key key, required this.controller})
-      : super(key: key);
 
-  @override
-  build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: controller, builder: (context, builder) {
-          return Stack(alignment:Alignment.center,
-          children : [ FloatingActionButton(child:Icon(FontAwesomeIcons.walkieTalkie) )] ))});
-
-    _open() {
-      controller.forward();
-    }
-
-    _close() {
-      controller.reverse();
-    }
-  }
-}
+// The Animation 
